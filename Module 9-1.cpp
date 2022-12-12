6aa2789856cda6ed0437a5bd8f6a7e1acfdc1a30
Andrey Gorelov <shorkun1974@yandex.ru>
2022-12-12 15:52:41 +0300

    первый запус к

--- Module 9-1.cpp
+++ Module 9-1.cpp
@@ -19,3 +19,41 @@
 Что оценивается
 Программа корректно считает и время внутри одного дня, и время поездки с прибытием на следующий день.
 Обеспечен контроль ввода и по длине строк, и по вводимым символам.*/
+
+#include <iostream>
+
+int main() {
+
+    std::string departureTime;
+    std::cout << "Input the departure time in the format (HH:MM): ";
+    std::cin >> departureTime;
+
+    bool incorrectTime = true, incorrectFormat = true;
+    int departureHours = 0, departureMinutes = 0;
+
+    do 
+    {
+        if (departureTime.length() != 5 || departureTime[2] != ':') {
+            std::cout << "Incorrect time format, input again: ";
+            std::cin >> departureTime;
+        }
+        else {
+            incorrectFormat = true;
+        }
+
+        departureHours = 10 * (departureTime[0] - '0') + (departureTime[1] - '0');
+        departureMinutes = 10 * (departureTime[3] - '0') + (departureTime[4] - '0');
+
+        if (departureHours > 24 || departureMinutes > 59) {
+            std::cout << "Incorrect time, input again: ";
+            std::cin >> departureTime;
+        }
+        else {
+            incorrectTime = true;
+        }
+
+    } while (incorrectTime || incorrectFormat);
+
+    std::cout << "time : " << departureHours << departureMinutes;
+
+}
